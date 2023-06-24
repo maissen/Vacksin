2e7111bf623c82207c3423362e389747b45b4a20
Maiss Dev <maissen.developer500@gmail.com>
2023-06-25 00:45:54 +0200

    Finishing the header section.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 document.getElementById('toggle_nav_links').onclick = function(){
     document.querySelector('.navbar__links').classList.toggle('active__liks_list');
 }
+
+window.onscroll = function(){
+    if(window.scrollY > 0){
+        document.getElementsByClassName('navbar')[0].classList.add('nav_links_scroll');
+    }else{
+        document.getElementsByClassName('navbar')[0].classList.remove('nav_links_scroll');
+    }
+
+    var header_badge = document.querySelectorAll('header .badge');
+    header_badge[0].style.transform = 'translateY(' + window.scrollY/3 + 'px)';
+    header_badge[1].style.transform = 'translateY(' + -window.scrollY-500 + 'px)';
+    header_badge[2].style.transform = 'translateY(' - window.scrollY/1.4 + 'px)';
+}
